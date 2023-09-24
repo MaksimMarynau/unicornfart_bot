ae6981a56a7110298c7d2e71ded358ba6581b76f
MaksimMarynau <maksim.marynau@gmail.com>
2023-09-24 12:34:23 +0200

    Wrap commands in Class
    Implement stop command
    Delete unused commands

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 import logging
 from telegram import Update
 from telegram.ext import filters, MessageHandler, ApplicationBuilder, CommandHandler, ContextTypes
-from telegram import InlineQueryResultArticle, InputTextMessageContent
-from telegram.ext import InlineQueryHandler
 from features import food_features
 from urllib.parse import urljoin
 import os
@@ -18,62 +16,51 @@ load_dotenv()
 
 TOKEN_ID = os.getenv('TOKEN_ID')
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Witam człowieku!")
 
-async def dinner_ideas(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    start_url = urljoin(PURE_URL, "/przepisy/dania-miesne/")
-    text = food_features.get_dinner_from_url(start_url)
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+class CommandManager:
+    def __init__(self):
+        self.run_command = True
 
-async def ideas_from_tag(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    tag_name = "".join(context.args) if len(context.args) < 1 else context.args[0] # support only 1 tag for now
-    tag_name = tag_name.lower()
-    text = food_features.get_tag_dishes(PURE_URL, tag_name)
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+    async def start(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        self.run_command = True
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Witam człowieku!")
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+    async def stop(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        if self.run_command:
+            await context.bot.send_message(chat_id=update.effective_chat.id, text="Na razie człowieku!")
+            self.run_command = False
+            return
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Uzyj /start, aby uruchomic bota.")
 
-async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text_caps = ' '.join(context.args).upper()
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+    async def dinner_ideas(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        if self.run_command:
+            start_url = urljoin(PURE_URL, "/przepisy/dania-miesne/")
+            text = food_features.get_dinner_from_url(start_url)
+            await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
 
-async def inline_caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    print(query)
-    query = update.inline_query.query
-    if not query:
-        return
-    results = []
-    results.append(
-        InlineQueryResultArticle(
-            id=query.upper(),
-            title='Caps',
-            input_message_content=InputTextMessageContent(query.upper())
-        )
-    )
-    await context.bot.answer_inline_query(update.inline_query.id, results)
+    async def ideas_from_tag(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        if self.run_command:
+            tag_name = "".join(context.args) if len(context.args) < 1 else context.args[0] # support only 1 tag for now
+            tag_name = tag_name.lower()
+            text = food_features.get_tag_dishes(PURE_URL, tag_name)
+            await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
 
-async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, nie wiem o co chodzi mordo.")
+    async def unknown(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        if self.run_command:
+            await context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, nie wiem o co chodzi mordo.")
+
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TOKEN_ID).build()
     
-    start_handler = CommandHandler('start', start)
-    caps_handler = CommandHandler('caps', caps)
-    tag_handler = CommandHandler('ideas_from_tag', ideas_from_tag)
-    dinner_ideas_handler = CommandHandler('dinner_ideas', dinner_ideas)
-    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
-    inline_caps_handler = InlineQueryHandler(inline_caps)
-    unknown_handler = MessageHandler(filters.COMMAND, unknown)
+    cm = CommandManager()
     
-    application.add_handler(start_handler)
-    application.add_handler(caps_handler)
-    application.add_handler(tag_handler)
-    application.add_handler(dinner_ideas_handler)
-    application.add_handler(echo_handler)
-    application.add_handler(inline_caps_handler)
-    application.add_handler(unknown_handler)
+    start_handler = CommandHandler('start', cm.start)
+    stop_handler = CommandHandler('stop', cm.stop)
+    tag_handler = CommandHandler('ideas_from_tag', cm.ideas_from_tag)
+    dinner_ideas_handler = CommandHandler('dinner_ideas', cm.dinner_ideas)
+    unknown_handler = MessageHandler(filters.COMMAND, cm.unknown)
+    
+    application.add_handlers([start_handler, stop_handler, tag_handler, dinner_ideas_handler, unknown_handler])
 
     application.run_polling()
